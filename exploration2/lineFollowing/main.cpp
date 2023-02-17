862220c99410cfd19d91257c9095cb03e764fbfe
ldfMion <72419437+ldfMion@users.noreply.github.com>
2023-02-17 16:30:20 -0500

    creation of LineSensor and Drive class

--- exploration2/lineFollowing/main.cpp
+++ exploration2/lineFollowing/main.cpp
@@ -3,32 +3,60 @@
 #include <FEHUtility.h>
 #include <FEHMotor.h>
 
-AnalogInputPin optoRight(FEHIO::P1_0);
-AnalogInputPin optoMiddle(FEHIO::P2_0);
-AnalogInputPin optoLeft(FEHIO::P3_0);
-
-FEHMotor leftMotor(FEHMotor::Motor0,9);
-FEHMotor rightMotor(FEHMotor::Motor1,9);
-
-
-
-/*
-class Sensor {
-    public:
-
-        AnalogInputPin sensor;
-        double onLine;
-        double offLine;
+class LineSensor
+{
+    private:
         double average;
-        double getValue(){
+        AnalogInputPin sensor;
+
+    public:
+        LineSensor(FEHIO::FEHIOPin port, double onLine, double offLine) : sensor(port)
+        {
+            average = (onLine + offLine) / 2;
+        }
+        double getValue()
+        {
             return sensor.Value();
         }
-        Sensor() {
+        bool isOnLine()
+        {
+            return getValue() > average;
+        }
+};
+
+class Drive {
+    private:
+        FEHMotor leftMotor;
+        FEHMotor rightMotor;
+    public: 
+        Drive(FEHMotor::FEHMotorPort leftPort, FEHMotor::FEHMotorPort rightPort) : leftMotor(leftPort, 9), rightMotor(rightPort, 9) {
 
         }
+        void straight()
+        {
+            leftMotor.SetPercent(25);
+            rightMotor.SetPercent(25);
+        }
 
-}
-*/
+        void right()
+        {
+            leftMotor.SetPercent(20);
+            rightMotor.SetPercent(10);
+        }
+
+        void left()
+        {
+            leftMotor.SetPercent(10);
+            rightMotor.SetPercent(20);
+        }
+
+};
+
+LineSensor optoLeft(FEHIO::P3_0, 2.33, 1.117);
+LineSensor optoMiddle(FEHIO::P2_0, 1.982, 0.768);
+LineSensor optoRight(FEHIO::P1_0, 2.201, 0.935);
+
+Drive drive(FEHMotor::Motor0, FEHMotor::Motor1);
 
 enum LineStates
 {
@@ -38,29 +66,9 @@ enum LineStates
 };
 
 
-void driveStraight() {
-    leftMotor.SetPercent(25);
-    rightMotor.SetPercent(25);
-}
-
-void driveRight() {
-    leftMotor.SetPercent(20);
-    rightMotor.SetPercent(10);
-}
-
-void driveLeft() {
-    leftMotor.SetPercent(10);
-    rightMotor.SetPercent(20);
-}
 
 int main(void)
 {
-    //int numSensors = 3;
-    //AnalogInputPin optoSensors[3] = {optoLeft, optoMiddle, optoRight};
-
-    double leftAvg = (2.33 + 1.117) / 2;
-    double middleAvg = (1.982 + 0.768) / 2;
-    double rightAvg = (2.201 + 0.935) / 2;
 
     int state = MIDDLE;
 
@@ -81,24 +89,27 @@ int main(void)
             case MIDDLE:
                 LCD.Clear(BLACK);
                 LCD.WriteLine("IS ON THE MIDDLE");
-                driveStraight();
-                if (optoRight.Value() > rightAvg)
+                drive.straight();
+                if (optoRight.isOnLine())
                 {
                     state = RIGHT; // update a new state
-                } else if (optoLeft.Value() > leftAvg){
+                }
+                else if (optoLeft.isOnLine())
+                {
                     state = LEFT;
                 }
-                
+
                 break;
             // If the right sensor is on the line...
             case RIGHT:
                 LCD.Clear(BLACK);
                 LCD.WriteLine("IS ON THE LEFT");
                 LCD.WriteLine("DRIVING RIGHT");
-                driveRight();
+                drive.right();
 
                 /* if I no longer need to turn right... */
-                if (optoRight.Value() < rightAvg) { /* update a new state */
+                if (!optoRight.isOnLine())
+                { /* update a new state */
                     state = MIDDLE;
                 }
                 break;
@@ -108,14 +119,15 @@ int main(void)
                 LCD.Clear(BLACK);
                 LCD.WriteLine("IS ON THE RIGHT");
                 LCD.WriteLine("DRIVING LEFT");
-                driveLeft();
+                drive.left();
                 /* if I no longer need to turn left... */
-                if (optoLeft.Value() < leftAvg) { /* update a new state */
+                if (!optoLeft.isOnLine())
+                { /* update a new state */
                     state = MIDDLE;
                 }
                 break;
             default: // Error. Something is very wrong.
-                LCD.Clear();
+                LCD.Clear(BLACK);
                 LCD.WriteLine("SOMETHING IS VERY WRONG");
                 break;
             }
@@ -126,4 +138,3 @@ int main(void)
 
     return 0;
 }
-
