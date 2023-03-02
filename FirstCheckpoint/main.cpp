04c728050a77004c0d3ea1a94f77b6f33b703d8e
Shritan Gunday <113409877+ShritanGunday@users.noreply.github.com>
2023-03-02 18:42:19 -0500

    Added main comments

--- FirstCheckpoint/main.cpp
+++ FirstCheckpoint/main.cpp
@@ -48,26 +48,43 @@ int main(void) {
     FEHServo rearServo(FEHServo::Servo1);
     AnalogInputPin cds(FEHIO::P1_1);
 
+    /* Resets the encoders to calibrate */
     leftEncoder.ResetCounts();
     rightEncoder.ResetCounts();
 
+    /* Waits until robot reads a value from the starting light */
     while (cds.Value() > 0.5) {
         LCD.Write("~[,,_,,]:3");
         LCD.Clear();
     }
+
+    /* Moves forward from the start to position to the rightmost ramp */
     moveForward(leftMotor, rightMotor, leftEncoder, rightEncoder, (int)(CLICKS_PER_INCH * 5.0), 25);
+    /* Turns to position straight to the rightmost ramp */
     turnLeft(leftMotor, rightMotor, leftEncoder, rightEncoder, (int)(CLICKS_PER_DEGREE * 42.0), 20);
+    /* Moves forward from the start, up the rightmost ramp, and right in front of the passport */
     moveForward(leftMotor, rightMotor, leftEncoder, rightEncoder, (int)(CLICKS_PER_INCH * 34.0), 25);
+    /* Turns left to position */
     turnLeft(leftMotor, rightMotor, leftEncoder, rightEncoder, (int)(CLICKS_PER_DEGREE * 90.0), 20);
+    /* Moves forward to position in front of the kiosk */
     moveForward(leftMotor, rightMotor, leftEncoder, rightEncoder, (int)(CLICKS_PER_INCH * 8.0), 25);
+    /* Turns left to position facing the kiosk */
     turnRight(leftMotor, rightMotor, leftEncoder, rightEncoder, (int)(CLICKS_PER_DEGREE * 100.0), 20);
+    /* Moves forward to touch the kiosk lightly */
     moveForward(leftMotor, rightMotor, leftEncoder, rightEncoder, (int)(CLICKS_PER_INCH * 16.0), 25);
+    /* Moves backwards to position */
     moveBackward(leftMotor, rightMotor, leftEncoder, rightEncoder, (int)(CLICKS_PER_INCH * 16.0), 25);
+    /* Turns left to position, rear of robot facing the wall next to the passport */
     turnLeft(leftMotor, rightMotor, leftEncoder, rightEncoder, (int)(CLICKS_PER_DEGREE * 90.0), 20);
+    /* Moves backwards to position, squaring up to the wall */
     moveBackward(leftMotor, rightMotor, leftEncoder, rightEncoder, (int)(CLICKS_PER_INCH * 20), 25);
+    /* Moves forward to position */
     moveForward(leftMotor, rightMotor, leftEncoder, rightEncoder, (int)(CLICKS_PER_INCH * 5.0), 25);
+    /* Turns right to position, rear of robot facing the start right above the rightmost ramp */
     turnRight(leftMotor, rightMotor, leftEncoder, rightEncoder, (int)(CLICKS_PER_DEGREE * 100.0), 20);
+    /* Moves backwards towards the start, stopping right before */
     moveBackward(leftMotor, rightMotor, leftEncoder, rightEncoder, (int)(CLICKS_PER_INCH * 35.0), 25);
+    /* Stops the robot */
     leftMotor.Stop();
     rightMotor.Stop();
 }
