7992bc9071eb4b258329a2f4d1ba9255d458b11c
ldfMion <72419437+ldfMion@users.noreply.github.com>
2023-04-11 16:12:15 -0400

    Update main.cpp

--- RPSTests/main.cpp
+++ RPSTests/main.cpp
@@ -2,6 +2,9 @@
 
 #include "Drive.h"
 
+// points
+#define LIGHT_COORDINATES {13.5,61.5}
+
 int main(void)
 {
     LCD.Clear(BLACK);
@@ -9,19 +12,28 @@ int main(void)
 
     Drive drive(FEHMotor::Motor0, FEHMotor::Motor1, FEHIO::P0_0, FEHIO::P0_1);
 
-    Position a = {0.0, 0.0};
+    Position light = LIGHT_COORDINATES;
 
-    LCD.WriteLine("Testing TurnTo");
-    drive.TurnTo(a);
+    while(true){
+        drive.checkInfo(light);
+        Sleep(2000);
+    }
 
     return 0;
 
-    LCD.WriteLine("Testing DriveTo");
-    drive.DriveTo(a);
-
-    LCD.WriteLine("Testing GoTo");
-    drive.GoTo(a);
+    LCD.WriteLine("Testing TurnTo");
+    drive.TurnTo(light);
 
     LCD.WriteLine("Testing GoToWithCorrection");
-    drive.GoTo(a);
+    drive.GoToWithCorrection(light);
+
+    LCD.WriteLine("Testing GoTo");
+    drive.GoTo(light);
+
+    
+
+    LCD.WriteLine("Testing DriveTo");
+    drive.DriveTo(light);
+
+
 }
