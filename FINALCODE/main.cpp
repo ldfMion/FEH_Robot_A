ffeff628cb81a71b63e5a66d04f8093d62062f78
ldfMion <72419437+ldfMion@users.noreply.github.com>
2023-04-17 21:01:53 -0400

    Update main.cpp

--- FINALCODE/main.cpp
+++ FINALCODE/main.cpp
@@ -33,10 +33,7 @@ AnalogInputPin cds(FEHIO::P1_1);
 #define SERVO_MIN 550
 
 #define COURSE_Y 72
-#define LIGHT_COORDINATES \
-    {                     \
-        10.5, COURSE_Y-11.5          \
-    }
+#define LIGHT_COORDINATES {13.3, 60.0}
 
 #define LEFTRAMP_COORDINATES \
     {                     \
@@ -55,23 +52,7 @@ Drive drive(FEHMotor::Motor0, FEHMotor::Motor1, FEHIO::P0_0, FEHIO::P0_1);
 void Ticket()
 {
 
-    leverServo.SetDegree(75);
-    /* Moves forward to position */
-    drive.Forward(2);
-    /* Turns right to position */
-    drive.TurnRight(90);
-    /* Moves forward to position */
-    drive.Forward(4);
-    /* Turns left to position, facing right-most ramp */
-    drive.TurnLeft(38);
-    // lowers lever, but enough for it to not hit the ramp
-    leverServo.SetDegree(75);
-    /* Moves forward up the ramp, stopping in front of the passport mechanism */
-    drive.SetDrivePercent(50);
-    drive.Forward(30);
-    drive.SetDrivePercent(25);
-
-    leverServo.SetDegree(15);
+    
     // Turn left to face the light
     Position light = LIGHT_COORDINATES;
     drive.GoToWithCorrection(light);
@@ -94,22 +75,8 @@ void Ticket()
         // Face the button
         drive.TurnLeft(90);
 
-        // Actually press the button
-        drive.BackTimed(2);
-        drive.Wiggle();
-        drive.BackTimed(1);
+        
 
-        leverServo.SetDegree(90);
-        // Drive back to the luggage
-        drive.ForwardTimed(6.5);
-
-        // Back into the passport wall
-        drive.Back(1);
-        drive.TurnRight(105);
-        leverServo.SetDegree(15);
-        drive.BackTimed(3.0);
-        drive.Wiggle();
-        drive.BackTimed(1.0);
     }
     else
     {
@@ -123,26 +90,12 @@ void Ticket()
         // Face the button
         drive.TurnLeft(90);
 
-        // Actually press the button
-        drive.BackTimed(2);
-        drive.Wiggle();
-        drive.BackTimed(1);
-
-        leverServo.SetDegree(90);
-
-        // Drive back to the luggage
-        drive.ForwardTimed(6.5);
-
-        // Back into the passport wall
-
-        drive.Back(1);
-
-        drive.TurnRight(105);
-        leverServo.SetDegree(15);
-        drive.BackTimed(4.0);
-        drive.Wiggle();
-        drive.BackTimed(1.0);
+        
     }
+    // Actually press the button
+    drive.BackTimed(2);
+    drive.Wiggle();
+    drive.BackTimed(1);
 }
 
 /**
@@ -150,8 +103,33 @@ void Ticket()
  * End Position: aligned with the passport task, facing west.
  */
 void Passport()
-{
-    /* Moves forward to position, parellel to passport */
+{   
+    /* Moves forward to position */
+    drive.Forward(2);
+    //lever up
+    leverServo.SetDegree(75);
+    /* Turns right to position */
+    drive.TurnRight(90);
+    /* Moves forward to position */
+    drive.Forward(4);
+    /* Turns left to position, facing right-most ramp */
+    drive.TurnLeft(38);
+    leverServo.SetDegree(15);
+    /* Moves forward up the ramp, stopping in front of the passport mechanism */
+    drive.SetDrivePercent(50);
+    drive.Forward(30);
+    drive.SetDrivePercent(25);
+
+    leverServo.SetDegree(15);
+
+    // Back into the passport wall
+    drive.TurnLeft(110);
+    leverServo.SetDegree(15);
+    drive.BackTimed(1.5);
+    drive.Wiggle();
+    drive.BackTimed(0.5);
+
+    /* Moves forward to position, parallel to passport */
     drive.Forward(7.8);
     /* Turns right to face passport, bar aligned with lever */
     drive.TurnRight(90);
@@ -159,15 +137,11 @@ void Passport()
     drive.Forward(1);
     /* Moves bar up */
     leverServo.SetDegree(90);
-    /* Move bar up while moving forward slowly until lever reaches top */
-    //drive.Forward(3);
-    drive.ForwardTimed(1.0);
-    /* Move backward a little */
-    drive.Back(2.5);
-    /* Turn left 90 degrees */
-    drive.TurnLeft(80);
-    /* Move bar down */
+    drive.ForwardTimed(2.0);
+    // move bar down
     leverServo.SetDegree(15);
+    /* Move backward a little */
+    drive.Back(4);
 }
 
 /**
@@ -176,17 +150,6 @@ void Passport()
  */
 void Luggage()
 {
-    drive.SetDrivePercent(40);
-
-    // Drive to be aligned with the luggage and the ramp.
-    drive.ForwardTimed(3.5);
-
-    drive.SetDrivePercent(25);
-
-    drive.Back(2);
-    drive.TurnLeft(90);
-    drive.Back(2);
-    
     Position leftRamp = LEFTRAMP_COORDINATES;
     drive.GoToWithCorrection(leftRamp);
 
@@ -209,8 +172,6 @@ void Luggage()
 
     Sleep(3.0);
     drive.Forward(3);
-
-    
 }
 
 /**
@@ -289,8 +250,8 @@ int main(void)
 
     LCD.Clear();
 
-    Ticket();
     Passport();
+    Ticket();
     Luggage();
     Levers();
     FinalButton();
