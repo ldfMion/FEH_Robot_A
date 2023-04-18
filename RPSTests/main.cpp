c18b08bbbb98f62f86e43760114a45d511ff9dbe
ldfMion <72419437+ldfMion@users.noreply.github.com>
2023-04-17 20:59:30 -0400

    Update main.cpp

--- RPSTests/main.cpp
+++ RPSTests/main.cpp
@@ -1,39 +1,261 @@
+/********************************/
+/*      Third checkpoint        */
+/*     OSU FEH Spring 2023      */
+/*           Team A             */
+/*          03/30/23            */
+/********************************/
+
+/* Include Preprocessor Directives */
 #include <FEHLCD.h>
+#include <FEHIO.h>
+#include <FEHUtility.h>
+#include <FEHMotor.h>
+#include <FEHServo.h>
+#include <FEHAccel.h>
+#include <FEHBattery.h>
+#include <FEHBuzzer.h>
+#include <FEHRPS.h>
+#include <FEHSD.h>
+#include <string.h>
+#include <stdio.h>
 
-#include "Drive.h"
+#include <Drive.h>
 
-// points
-#define LIGHT_COORDINATES {13.5,61.5}
+#define CLICKS_PER_INCH 400.0 / 12.0
+#define CLICKS_PER_DEGREE CLICKS_PER_INCH * 0.065
 
-int main(void)
-{
-    LCD.Clear(BLACK);
-    LCD.WriteLine("Hello, World!");
+FEHServo leverServo(FEHServo::Servo0);
+AnalogInputPin cds(FEHIO::P1_1);
 
-    Drive drive(FEHMotor::Motor0, FEHMotor::Motor1, FEHIO::P0_0, FEHIO::P0_1);
 
-    Position light = LIGHT_COORDINATES;
+// SERVO VALUES
+#define SERVO_MAX 2500
+#define SERVO_MIN 550
 
-    while(true){
-        drive.checkInfo(light);
-        Sleep(2000);
+#define COURSE_Y 72
+#define LIGHT_COORDINATES {13.3, 60.0}
+
+#define LEFTRAMP_COORDINATES \
+    {                     \
+        7, 45          \
     }
 
-    return 0;
+Drive drive(FEHMotor::Motor0, FEHMotor::Motor1, FEHIO::P0_0, FEHIO::P0_1);
 
-    LCD.WriteLine("Testing TurnTo");
-    drive.TurnTo(light);
 
-    LCD.WriteLine("Testing GoToWithCorrection");
+
+
+/**
+ * to be called from the start position. Completes the ticket task.
+ * End Position: backed up against passport wall.
+ */
+void Ticket()
+{
+
+    
+    // Turn left to face the light
+    Position light = LIGHT_COORDINATES;
     drive.GoToWithCorrection(light);
+    drive.TurnTo(135);
 
-    LCD.WriteLine("Testing GoTo");
-    drive.GoTo(light);
+    // Drive on top of the light
+    // drive.Forward(24);
+
+    LCD.Clear();
+    Sleep(2.0);
+    if (cds.Value() < 0.7)
+    {
+        LCD.Write("THE COLOR IS RED");
+        /* Touch the button */
+        drive.TurnLeft(45);
+
+        // drive to the button
+        drive.Back(8);
+
+        // Face the button
+        drive.TurnLeft(90);
+
+        
+
+    }
+    else
+    {
+        LCD.Write("THE COLOR IS BLUE");
+        /* Touch the button */
+        drive.TurnLeft(45);
+
+        // drive to the button
+        drive.Back(5);
+
+        // Face the button
+        drive.TurnLeft(90);
+
+        
+    }
+    // Actually press the button
+    drive.BackTimed(2);
+    drive.Wiggle();
+    drive.BackTimed(1);
+}
+
+/**
+ * to be called from the passport wall. Completes the passport task.
+ * End Position: aligned with the passport task, facing west.
+ */
+void Passport()
+{   
+    /* Moves forward to position */
+    drive.Forward(2);
+    //lever up
+    leverServo.SetDegree(75);
+    /* Turns right to position */
+    drive.TurnRight(90);
+    /* Moves forward to position */
+    drive.Forward(4);
+    /* Turns left to position, facing right-most ramp */
+    drive.TurnLeft(38);
+    leverServo.SetDegree(15);
+    /* Moves forward up the ramp, stopping in front of the passport mechanism */
+    drive.SetDrivePercent(50);
+    drive.Forward(30);
+    drive.SetDrivePercent(25);
+
+    leverServo.SetDegree(15);
+
+    // Back into the passport wall
+    drive.TurnLeft(110);
+    leverServo.SetDegree(15);
+    drive.BackTimed(1.5);
+    drive.Wiggle();
+    drive.BackTimed(0.5);
+
+    /* Moves forward to position, parallel to passport */
+    drive.Forward(7.8);
+    /* Turns right to face passport, bar aligned with lever */
+    drive.TurnRight(90);
+    /* Move bar up while moving forward slowly until lever reaches top */
+    drive.Forward(1);
+    /* Moves bar up */
+    leverServo.SetDegree(90);
+    drive.ForwardTimed(2.0);
+    // move bar down
+    leverServo.SetDegree(15);
+    /* Move backward a little */
+    drive.Back(4);
+}
+
+/**
+ * to be called from being aligned with the passport task. Completes the luggage task.
+ * End Position: at the bottom of the left ramp, facing south.
+ */
+void Luggage()
+{
+    Position leftRamp = LEFTRAMP_COORDINATES;
+    drive.GoToWithCorrection(leftRamp);
+
+    drive.TurnTo(270);
+    drive.TurnTo(270);
+
+    drive.SetDrivePercent(35);
+
+    drive.Forward(2);
+    Sleep(.5);
+    for (int i = 0; i < 8; i++) {
+        drive.Forward(.4);
+        Sleep(.5);
+    }
+    
+    drive.Forward(2);
+
+
+    drive.SetDrivePercent(25);
+
+    Sleep(3.0);
+    drive.Forward(3);
+}
+
+/**
+ * to be called from the bottom of the left ramp facing south. Completes the lever task.
+ * End Position: same place as start.
+ */
+void Levers()
+{
+
+    // Drive up to the lever
+    drive.Back(1);
+    // Put arm up
+    leverServo.SetDegree(100);
+    Sleep(2.0);
+    drive.Forward(3);
 
     
 
-    LCD.WriteLine("Testing DriveTo");
-    drive.DriveTo(light);
+    // Drop arm down
+    leverServo.SetDegree(20);
 
+    // Drive back
+    drive.Back(3);
 
+    // Wait the required time.
+    Sleep(5.0);
+
+    // Drive back forward, and lift the arm
+    drive.Forward(3);
+    leverServo.SetDegree(100);
+    Sleep(1.0);
+    
+    
+
+    // Drive back and lower the servo.
+    leverServo.SetDegree(15);
+    drive.Back(3);
+    
+}
+
+/**
+ * to be called from the bottom of the left ramp facing south. Completes the final button task.
+ * End Position: at the final button
+ */
+void FinalButton()
+{
+
+    drive.Forward(1.5);
+
+    // face the button
+    drive.TurnLeft(60);
+
+    // drive into it
+    drive.ForwardTimed(15.0);
+}
+
+/* Waits until robot reads a value from the starting light */
+void WaitToStart()
+{
+    LCD.Write("Waiting to start");
+    while (cds.Value() > 0.6)
+    {
+    }
+}
+
+int main(void)
+{
+
+    drive.initialize();
+
+    // sets servo values
+    leverServo.SetMin(SERVO_MIN);
+    leverServo.SetMax(SERVO_MAX);
+
+    WaitToStart();
+
+    LCD.Clear();
+
+    Passport();
+    Ticket();
+    Luggage();
+    Levers();
+    FinalButton();
+
+    LCD.Clear();
+    LCD.Write("Done!");
 }
